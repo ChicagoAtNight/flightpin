0ca6d0116d378b0b4665a4b2befa3ecde9f4931d
Alex Elman <chicagoatnite@gmail.com>
2011-10-09 15:24:52 -0500

    Putting together main page css, scripts, and date picker
    Signed-off-by: Alex Elman <chicagoatnite@gmail.com>

--- skypin_proj/skypin.py
+++ skypin_proj/skypin.py
@@ -21,7 +21,8 @@ import wsgiref.handlers
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 
-class Flight(db.Model):
+#class Flight(db.Model):
+#  def __init__(self):
   # airline =
   # arrives =
   # stop_1 =
@@ -44,48 +45,53 @@ class MainPage(webapp.RequestHandler):
     self.response.out.write('<!DOCTYPE HTML>')
     self.response.out.write('<html><body>')
     self.response.out.write('<head>')
-    self.response.out.write("""
-          <title>SkyPin: Pin your destination, find your flight fast</title>"""
+    self.response.out.write('<title>SkyPin: Pin your destination, find your flight fast</title>')
+    self.response.out.write('<script type="text/javascript" src="scripts/jquery-1.3.2.min.js>')
+    self.response.out.write('<script type="text/javascript" src="scripts/cal.js>')
+    self.response.out.write('''<script type="text/javascript">
+            jQuery(document).ready(function () {
+	    $('input.one').simpleDatepicker();
+            });
+          </script>''')
+    self.response.out.write('<link href="stylesheets/calendar.css" rel="stylesheet" type="text/css" />')
+    self.response.out.write('<link href="http://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet" type="text/css">')
     self.response.out.write('</head>')
-    greetings = db.GqlQuery("SELECT * "
-                            "FROM Flight "
-                            "ORDER BY price DESC LIMIT 20")
 
-    for greeting in greetings:
-      if greeting.author:
-        self.response.out.write('<b>%s</b> wrote:' % greeting.author.nickname())
-      else:
-        self.response.out.write('An anonymous person wrote:')
-      self.response.out.write('<blockquote>%s</blockquote>' %
-                              cgi.escape(greeting.content))
+    #greetings = db.GqlQuery("SELECT * "
+     #                       "FROM Flight "
+      #                      "ORDER BY price DESC LIMIT 20")
+
+#    for greeting in greetings:
+#      if greeting.author:
+#        self.response.out.write('<b>%s</b> wrote:' % greeting.author.nickname())
+#      else:
+#        self.response.out.write('An anonymous person wrote:')
+#      self.response.out.write('<blockquote>%s</blockquote>' %
+#                              cgi.escape(greeting.content))
 
 
-    self.response.out.write("""
-          <form>
-            <p><label>Departure date:</label></p>
-              <fieldset>
-                <legend></legend>
-              </fieldset>
- action="/sign" method="post">
-            <div><textarea name="content" rows="3" cols="60"></textarea></div>
-            <div><input type="submit" value="Sign Guestbook"></div>
-          </form>
+    self.response.out.write('''
+          <div id="dl1" class="datelabel">Select a departure date:</div>
+          <div id="calp1"><input id="seldate1" class="one" type="text" name="date" value="" /></div>
+          <div id="chb1" class="datelabel">One way only: <input id="onewaycheck" class="checkbox" ></div>
+          <div id="dl2" class="datelabel">Select a return date (or choose one-way):</div>
+          <div id="calp2"><input class="one" type="text" name="date" value="" /></div>
         </body>
-      </html>""")
+      </html>''')
 
 
-class Flights(webapp.RequestHandler):
-  def __init__(self):
+#class Flights(webapp.RequestHandler):
+#  def __init__(self):
 
-  def addFlight(self):
-    flight = Flight()
+#  def addFlight(self):
+#    flight = Flight()
 
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
+#    if users.get_current_user():
+#      greeting.author = users.get_current_user()
 
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/')
+#    greeting.content = self.request.get('content')
+#    greeting.put()
+#    self.redirect('/')
 
 
 application = webapp.WSGIApplication([
